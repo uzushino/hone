a0870e71c9beba420f7072ed4760d0a91db1a0da
uzushino <torattoria.mamiya@gmail.com>
2019-01-22 00:51:02 +0900

    Add support of three column.

--- src/query/column.rs
+++ src/query/column.rs
@@ -48,3 +48,17 @@ impl<A, B> Column for (A, B) where A: Column, B: Column {
         A::col_count() + B::col_count()
     }
 }
+
+impl<A, B, C> Column for (A, B, C) where A: Column, B: Column, C: Column {
+    fn cols(&self) -> String {
+        let ca = self.0.cols();
+        let cb = self.1.cols();
+        let cc = self.2.cols();
+
+        ca + ", " + &cb + ", " + &cc
+    }
+
+    fn col_count() -> usize {
+        A::col_count() + B::col_count() + C::col_count()
+    }
+}
--- tests/query/select_expr.rs
+++ tests/query/select_expr.rs
@@ -10,8 +10,8 @@ fn test_select() {
         let eq = eq_(a.user_id(), one);
         let q = q.where_(eq);
 
-        q.return_(a.user_id())
+        q.return_((a.user_id(), a.email(), val_(2)))
     });
 
-    assert_eq!(select(a.unwrap()).to_sql(), "SELECT User.user_id FROM User WHERE (User.user_id = 1)".to_string());
+    assert_eq!(select(a.unwrap()).to_sql(), "SELECT User.user_id, User.email, 2 FROM User WHERE (User.user_id = 1)".to_string());
 }
